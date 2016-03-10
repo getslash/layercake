c36cfa6675dec5543d8ea1df2d935dae8ba54086
Dror Levin <spatz@psybear.com>
2016-03-10 17:56:11 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,2 @@
+# LayerCake ![Travis build status](https://travis-ci.org/getslash/layercake.svg?branch=master)
+Configuration stach in Python
---
+++ tests/test_basic.py
@@ -0,0 +1,29 @@
+from layercake import LayerCake
+import pytest
+
+
+@pytest.fixture
+def lc():
+    return LayerCake(a=1, b=2)
+
+
+def test_assertions(lc):        # pylint: disable=redefined-outer-name
+    with pytest.raises(ValueError):
+        LayerCake()
+    with pytest.raises(ValueError):
+        lc.push()
+    with pytest.raises(ValueError):
+        lc.push(c=3)
+    with pytest.raises(IndexError):
+        lc.pop()
+
+
+def test_current(lc):           # pylint: disable=redefined-outer-name
+    assert lc.current.a == 1
+    with lc.overriding(a=3) as current:
+        assert current.a == 3
+        with pytest.raises(AttributeError):
+            current.a = 4
+    assert current != lc.current
+    assert lc.current.a == 1
+    assert dict(**lc.current) == {'a': 1, 'b': 2}
